8010f96a8d1d6937f583351e3f0a474e24a79e7d
Jessie41 <56587491+Jessie41@users.noreply.github.com>
2020-11-14 02:15:56 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,43 @@
-'use strict';
+
+const about = document.querySelector(".about");
+const portfolio = document.querySelector(".portfolio");
+const fac = document.querySelector(".fac");
+const contact = document.querySelector(".contact");
 
 
 
+const aboutPst = document.querySelector(".aboutPst");
+const portfolioPst = document.querySelector(".portfolioPst");
+const facPst = document.querySelector(".facPst");
+const contactPst = document.querySelector(".contactPst");
+
+
+
+about.addEventListener('click', (e) => {
+  aboutPst.scrollIntoView({
+    behavior: 'auto',
+    block: 'center',
+  });
+});
+
+portfolio.addEventListener('click', (e) => {
+  portfolioPst.scrollIntoView({
+    behavior: 'auto',
+    block: 'center',
+  });
+});
+
+
+fac.addEventListener('click', (e) => {
+  facPst.scrollIntoView({
+    behavior: 'auto',
+    block: 'center',
+  });
+});
+
+contact.addEventListener('click', (e) => {
+  contactPst.scrollIntoView({
+    behavior: 'auto',
+    block: 'center',
+  });
+});
